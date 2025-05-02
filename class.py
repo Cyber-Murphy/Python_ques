61f2cab01ab4c0d3395eeae818b72050ca37d221
Gaurav Singh <gv00001singh@gmail.com>
2025-05-02 13:53:58 +0530

    first commit

---
+++ class.py
@@ -0,0 +1,15 @@
+class Computer:
+    def config(self):
+        print('hello i am gaurav')
+
+comp1= Computer()
+# print('hello "',type(comp1))
+
+'''
+There are two ways to call config function
+
+
+1.Computer.config(comp1)
+2. comp1.config()
+'''
+comp1.config()
